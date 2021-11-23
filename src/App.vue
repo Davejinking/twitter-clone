a99e51d0160d136c339e3d2225c1d5e0f9d67c08
Davejinking <pinkies_umpire0j@icloud.com>
2021-11-23 17:05:23 +0900

    TODO 해결

--- src/App.vue
+++ src/App.vue
@@ -130,13 +130,13 @@
                             <!-- 메세지 화면 --> 
                             <div class="ml-2 flex-1 flex flex-col">
                                 <!-- 메세지 입력란 --> 
-                                <textarea placeholder="무슨 일이 일어나고 있나요?"
-                                    class="w-full text-lg font-bold focus:outline-none mb-3 resize-none">
-                                </textarea>
-
+                                <textarea
+                                    placeholder="무슨 일이 일어나고 있나요?"
+                                    class="w-full text-lg font-bold focus:outline-none mb-3 resize-none"
+                                ></textarea>
                                 <!-- 트윗버튼 --> 
                                 <div class="text-right">
-                                    <button class="bg-primary hover:bg-dark text-sm font-bold text-white px-4 py-2 rounded-full">트윗</button>
+                                    <button class="bg-primary hover:bg-dark text-sm font-bold text-white px-4 py-1 rounded-full">트윗</button>
                                 </div>
                             </div>
                         </div>
