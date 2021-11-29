0168ff148b0d5455426f7b44abde5ae380e83e86
Davejinking <pinkies_umpire0j@icloud.com>
2021-11-29 19:25:22 +0900

    firebase 프로젝트 생성

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -9,7 +9,7 @@ import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 
 export default {
-    components : { DefaultLayout, EmptyLayout },
+    components : { DefaultLayout, e },
     setup() {
         const route = useRoute()
         const layout = computed(() => route.meta.layout || 'DefaultLayout')
