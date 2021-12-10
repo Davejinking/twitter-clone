66df8b0acdb53182ef9f84028bffb0d150cfca37
Davejinking <pinkies_umpire0j@icloud.com>
2021-12-10 13:30:22 +0900

    프로필 페이지 - 트윗, 리트윗, 좋아요 트윗 가져오기

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -48,22 +48,26 @@
                     <span class="text-gray">{{ moment(currentUser.created_at).format('YYYY년 MM월 DD일') }}</span>
                 </div>
                 <div>
-                    <span class="font-bold mr-1">{{ currentUser.followings.length }}</span>
+                    <!-- <span class="font-bold mr-1">{{ currentUser.followings.length }}</span> -->
                     <span class="text-gray mr-3">팔로우 중</span>
-                    <span class="font-bold mr-1">{{ currentUser.followers.length }}</span>
+                    <!-- <span class="font-bold mr-1">{{ currentUser.followers.length }}</span> -->
                     <span class="text-gray">팔로우</span>
                 </div>
             </div>
             <!-- 탭 -->
             <div class="flex border-b bodrder-color mt-3">
-                <div class="py-3 text-primary font-bold border-b border-primary text-center w-1/4 hover:bg-blue-50 cursor-pointer">트윗</div>
-                <div class="py-3 text-gray border-primary text-center w-1/4 hover:bg-blue-50 cursor-pointer">트윗 및 답글</div>
-                <div class="py-3 text-gray border-primary text-center w-1/4 hover:bg-blue-50 cursor-pointer">미디어</div>
-                <div class="py-3 text-gray border-primary text-center w-1/4 hover:bg-blue-50 cursor-pointer">마음에 들어요</div>
+                <div @click="currentTab = 'tweet'" :class="`${currentTab == 'tweet' ? 'border-b border-primary text-primary' : 'text-gray'} py-3 font-bold text-center w-1/3 hover:bg-blue-50 cursor-pointer`">트윗</div>
+                <div @click="currentTab = 'retweet'" :class="`${currentTab == 'retweet' ? 'border-b border-primary text-primary' : 'text-gray'} py-3 font-bold text-center w-1/3 hover:bg-blue-50 cursor-pointer`">리트윗</div>
+                <div @click="currentTab = 'like'" :class="`${currentTab == 'like' ? 'border-b border-primary text-primary' : 'text-gray'} py-3 font-bold text-center w-1/3 hover:bg-blue-50 cursor-pointer`">좋아요</div>
             </div>
             <!-- 트윗  -->
             <div class="overflow-y-auto">
-                <Tweet v-for="tweet in tweets" :key="tweet.id" :currentUser="currentUser" :tweet="tweet"/>
+                <Tweet 
+                    v-for="tweet in currentTab == 'tweet' ? tweets : currentTab == 'retweet' ? reTweets : likeTweets" 
+                    :key="tweet.id" 
+                    :currentUser="currentUser"
+                    :tweet="tweet"
+                />
             </div>
         </div>
         <!-- 트렌드 섹션 -->
@@ -76,7 +80,7 @@ import Trends from '../components/Trends.vue'
 import Tweet from '../components/Tweet.vue'
 import store from '../store'
 import { computed, onBeforeMount, ref } from 'vue'
-import { TWEET_COLEECTION, USER_COLEECTION } from '../firebase'
+import { LIKE_COLEECTION, RETWEET_COLEECTION, TWEET_COLEECTION, USER_COLEECTION } from '../firebase'
 import getTweetInfo from '../utils/getTweetInfo'
 import moment from 'moment'
 
@@ -85,6 +89,9 @@ export default {
     setup() {
         const currentUser = computed(() => store.state.user)
         const tweets = ref([])
+        const reTweets = ref([])
+        const likeTweets = ref([])
+        const currentTab = ref('tweet')
 
         onBeforeMount(() => {
             // 스토어에 실시간반영
@@ -108,12 +115,51 @@ export default {
                 }
                 })
             })
+
+            // 리트윗 정보 가져오기
+            RETWEET_COLEECTION.where('uid', '==', currentUser.value.uid)
+            .orderBy('created_at', 'desc')
+            .onSnapshot((snapshot) => {
+                snapshot.docChanges().forEach(async (change) => {
+                const doc = await TWEET_COLEECTION.doc(change.doc.data().from_tweet_id).get()
+                let tweet = await getTweetInfo(doc.data(),  currentUser.value)
+
+                if (change.type === 'added') {
+                    reTweets.value.splice(change.newIndex, 0, tweet)
+                } else if (change.type === 'modified') {
+                    reTweets.value.splice(change.oldIndex, 1, tweet)
+                } else if (change.type === 'removed') {
+                    reTweets.value.splice(change.oldIndex, 1)
+                }
+                })
+            })
+
+            // 좋아요 정보 가져오기
+            LIKE_COLEECTION.where('uid', '==', currentUser.value.uid)
+            .orderBy('created_at', 'desc')
+            .onSnapshot((snapshot) => {
+                snapshot.docChanges().forEach(async (change) => {
+                const doc = await TWEET_COLEECTION.doc(change.doc.data().from_tweet_id).get()
+                let tweet = await getTweetInfo(doc.data(),  currentUser.value)
+
+                if (change.type === 'added') {
+                    likeTweets.value.splice(change.newIndex, 0, tweet)
+                } else if (change.type === 'modified') {
+                    likeTweets.value.splice(change.oldIndex, 1, tweet)
+                } else if (change.type === 'removed') {
+                    likeTweets.value.splice(change.oldIndex, 1)
+                }
+                })
+            })
         })
 
         return {
             currentUser,
             tweets,
             moment,
+            reTweets,
+            likeTweets,
+            currentTab,
         }
     }
 }
